0f793d530ab64745db51825dc71b59c922127949
Kolbe Weathington <kswof97@yahoo.com>
2018-10-10 02:37:49 -0500

    + Fullscreen
    Buggy

--- TigerHacksDisplay/src/Objects/Display.kt
+++ TigerHacksDisplay/src/Objects/Display.kt
@@ -1,29 +1,58 @@
+import javafx.embed.swing.JFXPanel
+import javafx.scene.Scene
 import javafx.scene.layout.Pane
-import java.awt.Dimension
+import javafx.scene.paint.Paint
+import javafx.scene.shape.Circle
+import java.awt.*
+import javax.swing.JFrame
 
-class Display() {
+class Display(){
+
 
     //RootPane for the display
-    var rootPane: Pane
+    var frame : JFrame
+    var panel: JFXPanel
+    var root : Pane
+    var window : Window
 
     //Variables for width and height
     var width: Double = 0.0
-        set(value) {
-            updateDisplay();
-        }
+
     var height: Double = 0.0
-        set(value) {
-            updateDisplay();
-        }
 
     //INITIALIZER///////////////////////////////////////////////////////////////////////////////////////////////////////
 
     init{
         var dimension = getWindowDimension();
-        rootPane = Pane();
+
+        frame = JFrame()
+        panel = JFXPanel();
+        root = Pane();
+        window = Window(frame)
+
+        //
+        var circ = Circle()
+        circ.radius = 400.0
+        circ.centerY = height
+        circ.centerX = width
+        circ.fill = Paint
+                .valueOf("Red")
+
+        root.children.addAll(circ)
+        //
         width = dimension!!.getWidth();
         height = dimension.getHeight();
-        updateDisplay();
+        println(width.toString() + " " + height)
+
+        frame.minimumSize = Dimension(Math.round(width).toInt(), Math.round(height).toInt())
+        panel.minimumSize = Dimension(Math.round(width).toInt(), Math.round(height).toInt())
+        root.setMinSize(width,height)
+
+        frame.add(panel);
+        panel.scene = Scene(root,width,height)
+        frame.pack()
+
+        initFullscreen();
     }
 
     //PUBLIC FUNCTIONS//////////////////////////////////////////////////////////////////////////////////////////////////
@@ -38,21 +67,60 @@ class Display() {
     //PRIVATE FUNCTIONS/////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    private fun updateDisplay() {
-        rootPane!!.setMinSize(this!!.width!!, this!!.height!!);
+    fun updateDisplay() {
+        var window = getWindowDimension()
+        width = window!!.getWidth()
+        height = window.getHeight()
+        initFullscreen()
     }
 
     private fun getWindowDimension(): Dimension?{
-        val windowDimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        var windowDimension = java.awt.Toolkit.getDefaultToolkit().screenSize;
         return windowDimension;
     }
 
     private fun testValues(){
-        println("RootPane = " + rootPane);
+        println("RootPane = " + root);
         println("Window Width: " + width);
         println("Window Height: " + height);
         println();
 
     }
+
+    //Functions for fullscreen setup
+
+    lateinit var myDevice : GraphicsDevice;
+    lateinit var screenDevices:Array<GraphicsDevice>;
+
+    private fun initFullscreen(){
+        screenDevices = GraphicsEnvironment.getLocalGraphicsEnvironment().screenDevices;
+        chooseScreenDevice()
+        if(checkDevice(myDevice)){
+            frame.isUndecorated = true;
+            frame.ignoreRepaint = true;
+            myDevice.fullScreenWindow = window;
+        }
+
+    }
+
+    private fun chooseScreenDevice(){
+        if(screenDevices!=null) {
+            for (device in screenDevices) {
+                println(device.iDstring)
+                if(device.iDstring.contains("Display0")){
+                    myDevice = device
+                }
+            }
+        }
+    }
+
+    private fun checkDevice(device:GraphicsDevice) : Boolean {
+        return device.isFullScreenSupported && device.isDisplayChangeSupported;
+    }
+
+//    private fun getDisplaySettings(device:GraphicsDevice):DisplayMode{
+//        var dispMode = DisplayMode(device.win)
+//    }
+
 
 }
--- TigerHacksDisplay/src/TigerHacksDisplay.java
+++ TigerHacksDisplay/src/TigerHacksDisplay.java
@@ -1,27 +1,35 @@
 import javafx.application.Application;
+import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+
 public class TigerHacksDisplay extends Application {
     Display disp;
+    JFrame frame;
+    JFXPanel panel;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         initDisplay();
-        Pane root = disp.getRootPane();
+        frame.setVisible(true);
 
-
-        Scene scene = new Scene(root, disp.getWidth(), disp.getHeight());
+        Scene scene = panel.getScene();
         scene.getStylesheets().add("../style.css");
 
         primaryStage.setTitle("THD");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        disp.updateDisplay();
     }
 
     private void initDisplay(){
         this.disp = new Display();
+        this.frame = disp.getFrame();
+        this.panel = disp.getPanel();
     }
 
     public static void main(String[] args){
